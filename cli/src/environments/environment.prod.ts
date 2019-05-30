0c15f3e06df011368bc22cc6ada4ea3d915509a4
baminmru <developer.bami@gmail.com>
2019-05-30 14:34:52 +0300

    fix api address

--- cli/src/environments/environment.prod.ts
+++ cli/src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: true,
   //baseAppUrl: 'http://localhost:63706/api'
-  baseAppUrl: 'http://localhost:5000/api'
+  //baseAppUrl: 'http://localhost:5000/api'
+  baseAppUrl: '/api'
   
 };
